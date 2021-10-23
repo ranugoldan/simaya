29ecdeecb6f030c8e9620b76df06ebe84e97f1a1
Juang <juangangger@gmail.com>
2021-10-23 19:24:34 +0700

    Added language resources for error messages when a Procurement cannot be found by id

--- resources/lang/en/admin/procurements/message.php
+++ resources/lang/en/admin/procurements/message.php
@@ -1,6 +1,8 @@
 <?php
 
 return array(
+  'does_not_exist' => 'Procurement does not exist.',
+
   'create' => array(
     'error'   		=> 'Procurement was not created, please try again. :(',
     'success' 		=> 'Procurement created successfully. :)'
--- resources/lang/id/admin/procurements/message.php
+++ resources/lang/id/admin/procurements/message.php
@@ -1,6 +1,8 @@
 <?php
 
 return array(
+  'does_not_exist' => 'Pengadaan tidak ditemukan.',
+
   'create' => array(
     'error'   		=> 'Pengadaan gagal dibuat, silakan coba lagi. :(',
     'success' 		=> 'Pengadaan berhasil dibuat. :)'
