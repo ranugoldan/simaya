76334af1ea6839b79f665fc78af7816863586e07
Juang <juangangger@gmail.com>
2021-11-03 12:54:58 +0700

    Added method to print form and other adjustments in web ProcurementsController

--- app/Http/Controllers/ProcurementsController.php
+++ app/Http/Controllers/ProcurementsController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\ImageUploadRequest;
 use App\Models\Procurement;
+use Carbon\Carbon;
+use PdfReport;
 
 class ProcurementsController extends Controller
 {
@@ -99,11 +104,27 @@ class ProcurementsController extends Controller
 
         $location_id                    = $request->input('location_id');
 
-        $procurement = $request->handleImages($procurement);
+        if ($request->has('asset_id')) {
+            $asset_id                   = $request->input('asset_id');
+        }
+
+        if ($request->has('procurement_form_delete') && $request->filled('procurement_form_delete')) {
+            try {
+                unlink(public_path().'/uploads/procurement_form/'.$procurement->procurement_form);
+                $procurement->procurement_form = NULL;
+            } catch (\Exception $e) {
+                \Log::info($e);
+            }
+        }
+
+        $procurement = $request->handleImages($procurement, 600, 'procurement_form', 'procurement_form', 'procurement_form');
 
         if ($procurement->save()) {
             $procurement->models()->sync($model_payload);
             $procurement->locations()->sync($location_id);
+            if (isset($asset_id)) {
+                $procurement->assets()->sync($asset_id);
+            }
             
             return redirect()->route("procurements.index")->with('success', trans('admin/procurements/message.update.success'));
         }
@@ -116,6 +137,14 @@ class ProcurementsController extends Controller
         $this->authorize('delete', Procurement::class);
         if (is_null($procurement = Procurement::find($procurementId))) {
             return redirect()->to(route('procurements.index'))->with('error', trans('admin/procurements/message.does_not_exist'));
+        }
+
+        if ($procurement->procurement_form) {
+            try {
+                Storage::disk('public')->delete('procurement_form/'.$procurement->procurement_form);
+            } catch (\Exception $e) {
+                \Log::error($e);
+            }
         }
 
         $procurement->delete();
@@ -140,6 +169,10 @@ class ProcurementsController extends Controller
 
         $procurement = Procurement::find($procurementId);
 
+        if (isset($procurement->procurement_form)) {
+            return redirect()->route('procurements.index')->with('error', trans('admin/procurements/message.approval_uploaded'));
+        }
+
         if (isset($procurement->id)) {
             return view('procurements/approve', compact('procurement'));
         }
@@ -157,9 +190,12 @@ class ProcurementsController extends Controller
 
         $procurement = $request->handleImages($procurement, 600, 'procurement_form', 'procurement_form', 'procurement_form');
 
-        if (isset($procurement->procurement_form)) {
+        if (isset($procurement->procurement_form) && $procurement->status != 3) {
             $procurement->status = 2;
+            $procurement->approved_by = Auth::id();
+            $procurement->approved_at = Carbon::now()->toDateTimeString();
         }
+
 
         if ($procurement->save()) {
             return redirect()->route("procurements.index")->with('success', trans('admin/procurements/message.approve.success'));
@@ -173,6 +209,10 @@ class ProcurementsController extends Controller
         $this->authorize('assign', Procurement::class);
 
         $procurement = Procurement::find($procurementId);
+
+        if (is_null($procurement->procurement_form)) {
+            return redirect()->route('procurements.index')->with('error', trans('admin/procurements/message.approval_not_uploaded'));
+        }
 
         if (isset($procurement->id)) {
             return view('procurements/assign', compact('procurement'));
@@ -191,8 +231,10 @@ class ProcurementsController extends Controller
 
         $asset_ids = collect($request->asset_id);
 
-        if (($procurement->models->count()) == $asset_ids->count()) {
+        if ((($procurement->models->count()) == $asset_ids->count()) && isset($procurement->procurement_form)) {
             $procurement->status = 3;
+            $procurement->assigned_by = Auth::id();
+            $procurement->assigned_at = Carbon::now()->toDateTimeString();
         }
 
         if ($procurement->save()) {
@@ -202,5 +244,60 @@ class ProcurementsController extends Controller
         }
 
         return redirect()->back()->withInput()->withErrors($procurement->getErrors());
+    }
+
+    public function print($procurementId = null)
+    {
+        $this->authorize('view', Procurement::class);
+
+        if (is_null($procurement = Procurement::find($procurementId))) {
+            return redirect()->route('procurements.index')->with('error', trans('admin/procurements/message.does_not_exist'));
+        }
+
+        $locations = $procurement->locations;
+        $locations_str = '';
+
+        foreach ($locations as $idx => $location) {
+            if ($idx == 0) {
+                $locations_str .= $location->name;
+            } else {
+                $locations_str .= ', '.$location->name;
+            }
+        }
+
+        $title = 'Pengadaan '.$procurement->procurement_tag;
+
+        $meta = [
+            'No. Pengajuan'     => $procurement->procurement_tag,
+            'Tanggal Pengajuan' => $procurement->created_at,
+            'Vendor'            => $procurement->supplier->name,
+            'Unit/Bidang'       => $procurement->department->name,
+            'Posisi'            => $locations_str,
+        ];
+
+        $query = DB::table('procurement_models')
+                    ->join('procurements', 'procurement_models.procurement_id', '=', 'procurements.id')
+                    ->join('models', 'procurement_models.model_id', '=', 'models.id')
+                    ->join('users', 'procurements.user_id', '=', 'users.id')
+                    ->select('procurement_models.*', 'models.name', 'users.first_name', 'users.last_name', 'users.jobtitle')
+                    ->where('procurement_models.procurement_id', '=', $procurementId);
+
+        $columns = [
+            'Nama Barang' => function($result) {
+                return $result->name;
+            },
+            'qty',
+            'Harga' => function($result) {
+                return \App\Helpers\Helper::formatCurrencyOutput($result->purchase_cost);
+            },
+            'Jumlah' => function($result) {
+                return \App\Helpers\Helper::formatCurrencyOutput($result->qty * $result->purchase_cost);
+            }
+        ];
+
+        return PdfReport::of($title, $meta, $query, $columns)
+                        ->editColumns(['Harga', 'Jumlah'], ['class' => 'right'])
+                        ->showTotal(['Jumlah' => 'point'])
+                        ->download($title);
     }
 }
